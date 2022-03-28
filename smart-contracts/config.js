b7541c5c67f9bd1583a862238a48a7600cec604f
10dimensions <relay.10d@gmail.com>
2022-03-28 15:43:23 +0530

    Deploy contracts to Mumbai Testnet

---
+++ smart-contracts/config.js
@@ -0,0 +1,3 @@
+
+  export const marketplaceAddress = "0xd2107Fc5fB041e0b2cF4091B7aC0678f81A5F8cA"
+  
--- smart-contracts/hardhat.config.js
+++ smart-contracts/hardhat.config.js
@@ -7,11 +7,10 @@ module.exports = {
     hardhat: {
       chainId: 1337
     },
-//  unused configuration commented out for now
-//  mumbai: {
-//    url: "https://rpc-mumbai.maticvigil.com",
-//    accounts: [process.env.privateKey]
-//  }
+ mumbai: {
+   url: "https://rpc-mumbai.maticvigil.com",
+   accounts: ["3d6686006cb1682ced3494f863ed7fc341d1ff2c37c211f5c401f2b935bdbaf3"]
+ }
   },
   solidity: {
     version: "0.8.4",
